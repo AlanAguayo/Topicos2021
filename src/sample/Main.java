06255ffd56248d00b78ebf443fdc8b2e6eb98fc9
AlanAguayo <79882490+AlanAguayo@users.noreply.github.com>
2021-03-11 18:49:11 -0600

    Clase6

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,30 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
-import java.awt.*;
+import sample.view.Calculadora;
+import sample.view.Rompecabezas;
 
 
 public class Main extends Application {
 
+    private VBox vBox;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1,menCompetencia2,menCerrar;
+    private MenuItem mitCalcu, mitRompeCabezas,mniSalir;
+    private Scene escena;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        CrearUI(primaryStage);
+        CrearMenu();
 
-
+        primaryStage.setTitle("Proyecto TAP");
+        primaryStage.setScene(escena);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 
 
@@ -27,16 +32,59 @@ public class Main extends Application {
         launch(args);
     }
 
+    //Aguayo Sanchez Alan Giovanni
+
+    private void CrearMenu(){
+        vBox = new VBox();
+
+        mnbPrincipal = new MenuBar();
+        menCompetencia1=new Menu("Competencia 1");
+        menCompetencia2=new Menu("Competencia 2");
+        menCerrar=new Menu("Cerrar");
+        mnbPrincipal.getMenus().addAll(menCompetencia1,menCompetencia2,menCerrar);
+
+        mitCalcu =new MenuItem("Calculadora");
+        mitCalcu.setOnAction(event -> opcionesMenu(1));
+        mitRompeCabezas = new MenuItem("Rompecabezas");
+        mitRompeCabezas.setOnAction(event -> opcionesMenu(2));
+        menCompetencia1.getItems().addAll(mitCalcu,mitRompeCabezas);
+
+        mniSalir=new MenuItem("Salir");
+        mniSalir.setOnAction(event -> System.exit(0));
+        menCerrar.getItems().add(mniSalir);
+
+        vBox.getChildren().add(mnbPrincipal);
+
+        escena = new Scene(vBox,300,70);
+    }
+
+    private void opcionesMenu(int opc) {
+        switch(opc){
+            case 1:new Calculadora();
+            break;
+            case 2:new Rompecabezas();
+            break;
+        }
+    }
 
     private void CrearUI(Stage primaryStage){
 
+        //Menu
+
+
+
+
+
         //Parte 1 hbox,vbox
-        Button btn1,btn2,btn3;
+       /* Button btn1,btn2,btn3;
 
         btn1=new Button("Boton 1");
         btn2=new Button("Boton 2");
         btn3=new Button("Boton 3");
+*/
 
+        //HBox
+        /*
         HBox hBox = new HBox();
 
         hBox.getChildren().addAll(btn1,btn2,btn3);
@@ -45,6 +93,18 @@ public class Main extends Application {
 
         Scene scene = new Scene(hBox,400,70);
 
+
+         */
+
+        //BorderPane
+       /*
+        BorderPane border = new BorderPane();
+        Label lbl=new Label("Etiqueta");
+        border.setTop(lbl);
+        Scene scene = new Scene(border);
+*/
+
+        //VBox
         /*
 
         VBox vBox = new VBox();
@@ -58,15 +118,18 @@ public class Main extends Application {
 
         Scene scene = new Scene(vBox,400,70);
 
-
+*/
 
         //Parte 2 flowpane
+        /*
         FlowPane flow = new FlowPane(Orientation.VERTICAL);
         flow.getChildren().addAll(btn1,btn2,btn3);
 
         Scene scene= new Scene(flow,400,70);
+*/
 
         //Parte 3 gridpane
+        /*
         GridPane grid = new GridPane();
         grid.add(new Button("Posicion 0, 0"), 0, 0);
         grid.add(new Button("Posicion 1, 0"), 1, 0);
@@ -76,8 +139,10 @@ public class Main extends Application {
         grid.add(new Button("Posicion 1, 2"), 1, 2);
         grid.add(new Button("Posicion Col 2 Fila 0"), 2, 2);
         Scene scene=new Scene(grid);
+        */
 
         //Parte 4 anchorpane
+        /*
         AnchorPane anchor = new AnchorPane();
 
         AnchorPane.setTopAnchor(btn1,15.0);
@@ -87,14 +152,18 @@ public class Main extends Application {
 
         anchor.getChildren().addAll(btn1,btn2,btn3);
 
-       Scene scene=new Scene(anchor);
+        Scene scene=new Scene(anchor);
+        */
 
         //Parte 5 stackpane
+        /*
         StackPane stack = new StackPane();
         stack.getChildren().addAll(btn1,btn2,btn3);
         Scene scene =new Scene(stack);
+*/
 
         //Parte 6 titlepane
+        /*
         TilePane tile = new TilePane();
         tile.setHgap(8);
         tile.setPrefColumns(4);
@@ -104,17 +173,23 @@ public class Main extends Application {
         */
 
         //Parte 7 color y tamaño
-
+/*
         btn1.setMaxWidth(100);
         btn1.setMaxHeight(200);
-
         btn1.setStyle("-fx-background-color: #00ff00");
 
+        btn2.setMaxWidth(50);
+        btn2.setMaxHeight(100);
+        btn2.setStyle("-fx-background-color: #000000");
+
+        btn3.setMaxWidth(150);
+        btn3.setMaxHeight(250);
+        btn3.setStyle("-fx-background-color: #ff2d00");
 
 
-        primaryStage.setTitle("Proyecto TAP");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+        */
+
 
     }
 
---
+++ src/sample/view/Calculadora.java
@@ -0,0 +1,93 @@
+package sample.view;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+
+public class Calculadora extends Stage implements EventHandler {
+    
+    private Scene escena;
+    private TextField txtOperacion;
+    private HBox[] hBoxes;
+    private Button[] arBotones;
+    private VBox vBox;
+    private char[] arNumeros = {'7','8','9','/','4','5','6','*','1','2','3','+','0','.','=','-'};
+    
+    public Calculadora() {
+        CrearUI();
+        
+        this.setTitle("Calculadora");
+        this.setScene(escena);
+        this.show();
+    }
+
+    private void CrearUI() {
+        vBox = new VBox();
+        hBoxes = new HBox[4];
+        arBotones = new Button [16];
+
+        txtOperacion = new TextField();
+        txtOperacion.setEditable(false);
+        txtOperacion.setPrefHeight(50);
+        txtOperacion.setText("0");
+
+        int pos=0;
+        for(int i=0;i< hBoxes.length;i++){
+            hBoxes[i]=new HBox();
+            hBoxes[i].setSpacing(10);
+            hBoxes[i].setPadding(new Insets(5));
+            for (int j = 0; j < 4; j++) {
+                arBotones[pos] = new Button(arNumeros[pos]+"");
+                //arBotones[pos].addEventHandler(MouseEvent.MOUSE_CLICKED,new EventoCalcu(arNumeros[pos]));
+                arBotones[pos].addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        System.out.println("Tercer Evento");
+                    }
+                });
+                arBotones[pos].setPrefSize(50,50);
+                hBoxes[i].getChildren().add(arBotones[pos]);
+                pos++;
+            }
+        }
+        vBox.getChildren().addAll((txtOperacion),hBoxes[0],hBoxes[1],hBoxes[2],hBoxes[3]);
+        vBox.setPadding(new Insets(8,5,5,5));
+
+        for(int i=0;i< arBotones.length;i++){
+            arBotones[i]=new Button();
+        }
+
+
+
+        escena = new Scene(vBox,250,250);
+    }
+
+    @Override
+    public void handle(Event event) {
+        System.out.println("Primer Evento");
+    }
+
+    class EventoCalcu implements EventHandler{
+
+        char tecla;
+        public EventoCalcu(char tecla){
+            this.tecla=tecla;
+        }
+
+        @Override
+        public void handle(Event event) {
+            System.out.println("Segundo Evento");
+        }
+    }
+}
