7962c3e0dcf3405bc81526aeccf97717d75aed87
AlanAguayo <79882490+AlanAguayo@users.noreply.github.com>
2021-04-08 18:27:29 -0500

    Topicos

--- src/sample/view/Rompecabezas.java
+++ src/sample/view/Rompecabezas.java
@@ -14,10 +14,17 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 
 public class Rompecabezas extends Stage implements EventHandler {
 
-    private String arImagenes[] = {"fila-1-col-1.jpg", "fila-1-col-2.jpg", "fila-1-col-3.jpg", "fila-1-col-1 (1).jpg", "fila-1-col-2 (1).jpg", "fila-1-col-3 (1).jpg", "fila-1-col-1 (2).jpg", "fila-1-col-2 (2).jpg", "fila-1-col-3 (2).jpg",};
+    private String arImagenes2[] = {"2x2fila-1-col-1.jpg", "2x2fila-1-col-2.jpg", "2x2fila-2-col-1.jpg", "2x2fila-2-col-2.jpg"};
+    private String arImagenes3[] = {"3x3fila-1-col-1.jpg", "3x3fila-1-col-2.jpg", "3x3fila-1-col-3.jpg", "3x3fila-2-col-1.jpg", "3x3fila-2-col-2.jpg", "3x3fila-2-col-3.jpg", "3x3fila-3-col-1.jpg", "3x3fila-3-col-2.jpg", "3x3fila-3-col-3.jpg"};
+    private String arImagenes4[] = {"4x4fila-1-col-1.jpg", "4x4fila-1-col-2.jpg", "4x4fila-1-col-3.jpg", "4x4fila-1-col-4.jpg", "4x4fila-2-col-1.jpg", "4x4fila-2-col-2.jpg", "4x4fila-2-col-3.jpg", "4x4fila-2-col-4.jpg", "4x4fila-3-col-1.jpg", "4x4fila-3-col-2.jpg", "4x4fila-3-col-3.jpg", "4x4fila-3-col-4.jpg", "4x4fila-4-col-1.jpg", "4x4fila-4-col-2.jpg", "4x4fila-4-col-3.jpg", "4x4fila-4-col-4.jpg"};
+    private String arImagenes[];
+    private String arImagenesAux[];
+
     private String arAsignacion[][];
 
     private BorderPane borderPane;
@@ -62,14 +69,32 @@ public class Rompecabezas extends Stage implements EventHandler {
     public void handle(Event event) {
         noPiezas = Integer.parseInt(txtTarjetas.getText());
         btnTarjetas = new Button[noPiezas][noPiezas];
-        arAsignacion = new String[3][3];
+        arAsignacion = new String[noPiezas][noPiezas];
 
+        switch (noPiezas) {
+            case 2:
+                arImagenes = arImagenes2;
+                arImagenesAux = new String[arImagenes2.length];
+                break;
+            case 3:
+                arImagenes = arImagenes3;
+                arImagenesAux = new String[arImagenes3.length];
+                break;
+            case 4:
+                arImagenes = arImagenes4;
+                arImagenesAux = new String[arImagenes4.length];
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "No hay rompecabezas de ese tamaño");
+                break;
+        }
         revolver();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < noPiezas; i++) {
+            for (int j = 0; j < noPiezas; j++) {
 
-                Image img = new Image("sample/assets/" + arAsignacion[i][j]);
+                Image img = new Image("sample/assets/Rompecabezas/" + arAsignacion[i][j]);
+
                 ImageView imv = new ImageView(img);
                 imv.setFitHeight(145);
                 imv.setPreserveRatio(true);
@@ -82,6 +107,9 @@ public class Rompecabezas extends Stage implements EventHandler {
 
 
                 tablero.add(btnTarjetas[i][j], j, i);
+
+
+
             }
         }
 
@@ -93,13 +121,15 @@ public class Rompecabezas extends Stage implements EventHandler {
             xTemp = i;
             yTemp = j;
         } else {
-            Image img = new Image("sample/assets/" + arAsignacion[i][j]);
+
+
+            Image img = new Image("sample/assets/Rompecabezas/" + arAsignacion[i][j]);
             ImageView imv = new ImageView(img);
             imv.setFitHeight(145);
             imv.setPreserveRatio(true);
 
 
-            Image img2 = new Image("sample/assets/" + arAsignacion[xTemp][yTemp]);
+            Image img2 = new Image("sample/assets/Rompecabezas/" + arAsignacion[xTemp][yTemp]);
             ImageView imv2 = new ImageView(img2);
             imv2.setFitHeight(145);
             imv2.setPreserveRatio(true);
@@ -109,23 +139,27 @@ public class Rompecabezas extends Stage implements EventHandler {
             btnTarjetas[i][j].setGraphic(imv2);
 
             String asignacionTemp = arAsignacion[i][j];
-            arAsignacion[i][j]=arAsignacion[xTemp][yTemp];
-            arAsignacion[xTemp][yTemp]=asignacionTemp;
+            arAsignacion[i][j] = arAsignacion[xTemp][yTemp];
+            arAsignacion[xTemp][yTemp] = asignacionTemp;
 
             bandera = !bandera;
+
         }
+        comprobacion();
     }
 
     private void revolver() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+
+
+        for (int i = 0; i < noPiezas; i++) {
+            for (int j = 0; j < noPiezas; j++) {
                 arAsignacion[i][j] = new String();
             }
         }
         int posx, posy = 0;
         for (int i = 0; i < arImagenes.length; ) {
-            posx = (int) (Math.random() * 3);
-            posy = (int) (Math.random() * 3);
+            posx = (int) (Math.random() * noPiezas);
+            posy = (int) (Math.random() * noPiezas);
 
             if (arAsignacion[posx][posy].equals("")) {
                 arAsignacion[posx][posy] = arImagenes[i];
@@ -133,6 +167,42 @@ public class Rompecabezas extends Stage implements EventHandler {
             }
 
         }
+
+    }
+
+    private void comprobacion() {
+        boolean comp = true;
+
+
+        int cont = 0;
+        for (int i = 0; i < noPiezas; i++) {
+            for (int j = 0; j < noPiezas; j++) {
+
+                arImagenesAux[cont] = arAsignacion[i][j];
+
+                cont++;
+
+            }
+        }
+
+        int i = 0;
+
+
+        while (comp == true && i < arImagenes.length) {
+            for (i = 0; i < arImagenes.length; i++) {
+
+                if (!arImagenesAux[i].equals(arImagenes[i])) {
+
+                    comp = false;
+                }
+            }
+        }
+
+
+        if (comp) {
+            JOptionPane.showMessageDialog(null, "Termino el rompecabezas. Felicidades!!!");
+        }
+
 
     }
 }
