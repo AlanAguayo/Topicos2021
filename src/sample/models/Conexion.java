b1752d325e6a8a45b37168d7819697a0c1b24362
AlanAguayo <79882490+AlanAguayo@users.noreply.github.com>
2021-04-22 18:56:08 -0500

    baseDatos

---
+++ src/sample/models/Conexion.java
@@ -0,0 +1,24 @@
+package sample.models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+    private static String server = "127.0.0.1";
+    private static String user   = "alan";
+    private static String pwd    = "123";
+    private static String db     = "musicadb";
+
+    private static Connection conexion;
+    public static void getConexion(){
+        try{
+
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mariadb://"+server+"3306/"+db,user,pwd);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
